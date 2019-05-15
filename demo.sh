3e90bf5937bec3519cb6a729928c88794e31c851
king019 <king019@users.noreply.github.com>
2019-05-15 23:40:59 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # apollo config db info
-apollo_config_db_url=jdbc:mysql://docker_mysql:3306/ApolloConfigDB?characterEncoding=utf8
+apollo_config_db_url=jdbc:mysql://mysql:3306/ApolloConfigDB?characterEncoding=utf8
 apollo_config_db_username=root
 apollo_config_db_password=111111
 
 # apollo portal db info
-apollo_portal_db_url=jdbc:mysql://docker_mysql:3306/ApolloPortalDB?characterEncoding=utf8
+apollo_portal_db_url=jdbc:mysql://mysql:3306/ApolloPortalDB?characterEncoding=utf8
 apollo_portal_db_username=root
 apollo_portal_db_password=111111
 
